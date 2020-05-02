8c79c095fb36f27815f16a4db2ff2e3f134b6517
Loogeek <xiaolong3956@gmail.com>
2020-05-02 10:52:33 +0800

    feat: add beforeUpload and onChange to Upload component

--- src/components/Upload/upload.tsx
+++ src/components/Upload/upload.tsx
@@ -6,6 +6,7 @@ import Button from '../Button';
 export interface IUploadProps {
   action: string;
   defaultFileList?: File[];
+  beforeUpload?: (file: File) => boolean | Promise<File>;
   onChange?: (file: File) => void;
   onRemove?: (file: File) => void;
   onSuccess?: (data: any, file: File) => void;
@@ -14,7 +15,7 @@ export interface IUploadProps {
 }
 
 export const Upload: FC<IUploadProps> = (props) => {
-  const { action, onSuccess, onError, onProgress } = props;
+  const { action, beforeUpload, onChange, onSuccess, onError, onProgress } = props;
   const inputRef = useRef<HTMLInputElement>(null);
 
   const handleUpload = () => {
@@ -36,33 +37,55 @@ export const Upload: FC<IUploadProps> = (props) => {
   const uploadFiles = (files: FileList) => {
     const fileList = Array.from(files);
     fileList.forEach((file) => {
-      const formData = new FormData();
-      formData.append(file.name, file);
-      axios
-        .post(action, formData, {
-          headers: {
-            'Content-Type': 'multipart/form-data',
-          },
-          onUploadProgress: (e) => {
-            const percentage = Math.round((e.loaded * 100) / e.total) || 0;
-            if (percentage < 100) {
-              if (onProgress) {
-                onProgress(percentage, file);
-              }
-            }
-          },
-        })
-        .then((data) => {
-          if (onSuccess) {
-            onSuccess(data, file);
-          }
-        })
-        .catch((err) => {
-          if (onError) {
-            onError(err, file);
-          }
-        });
+      if (!beforeUpload) {
+        postFile(file);
+      } else {
+        const result = beforeUpload(file);
+
+        if (result && result instanceof Promise) {
+          result.then((processedFile) => postFile(processedFile));
+        } else if (result !== false) {
+          postFile(file);
+        }
+      }
     });
+  };
+
+  const postFile = (file: File) => {
+    const formData = new FormData();
+    formData.append(file.name, file);
+    axios
+      .post(action, formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+        onUploadProgress: (e) => {
+          const percentage = Math.round((e.loaded * 100) / e.total) || 0;
+          if (percentage < 100) {
+            if (onProgress) {
+              onProgress(percentage, file);
+            }
+          }
+        },
+      })
+      .then((data) => {
+        if (onSuccess) {
+          onSuccess(data, file);
+        }
+
+        if (onChange) {
+          onChange(file);
+        }
+      })
+      .catch((err) => {
+        if (onError) {
+          onError(err, file);
+        }
+
+        if (onChange) {
+          onChange(file);
+        }
+      });
   };
 
   return (
